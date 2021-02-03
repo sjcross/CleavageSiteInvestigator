a5fab69dba09bf9d6a3ff1cccc80800678545012
Stephen Cross <stephencross86@gmail.com>
2021-02-03 08:38:48 +0000

    Added rotation method to animation
    Updated animation to include searching for RC cassette start

--- src/animation.py
+++ src/animation.py
@@ -8,7 +8,8 @@ win_h=600
 fps = 30
 step_size = 0.01
 
-bubble_outline = "blue"
+bubble_outline_sense = "blue"
+bubble_outline_rc = "red"
 bubble_width = 3
 bubble_w = 0.05
 bubble_h = 0.03
@@ -77,8 +78,11 @@ def create_text(canvas, x, y, text, font, tags=[]):
 def create_bubble(canvas, x1, y1, x2, y2, bubble_outline="black", bubble_width=1, dna_fill="black", dna_width=1, tags=[]):
     yc = y1 + (y2-y1)/2
     canvas.create_line(win_w*x1, win_h*yc, win_w*x2, win_h*yc, fill=dna_fill, width=dna_width, tags=tags)
-    canvas.create_rectangle(win_w*x1, win_h*y1, win_w*x2, win_h*y2,outline=bubble_outline,width=bubble_width,tags=tags)
-
+    canvas.create_line(win_w*x1, win_h*y1, win_w*x2, win_h*y1, fill=bubble_outline, width=bubble_width, tags=tags)
+    canvas.create_line(win_w*x2, win_h*y1, win_w*x2, win_h*y2, fill=bubble_outline, width=bubble_width, tags=tags)
+    canvas.create_line(win_w*x2, win_h*y2, win_w*x1, win_h*y2, fill=bubble_outline, width=bubble_width, tags=tags)
+    canvas.create_line(win_w*x1, win_h*y2, win_w*x1, win_h*y1, fill=bubble_outline, width=bubble_width, tags=tags)
+    
 def animate_pause(window,duration):
     n_frames = duration*fps
     window.update() # Only needs doing once, since nothing changes
@@ -98,29 +102,52 @@ def animate_motion(window,canvas, x1, y1, x2, y2, step_size, tag):
         window.update()
         time.sleep(1/fps)
 
+def animate_rotation(window, canvas, cx, cy, angle_degs, duration, tag):
+    n_frames = duration*fps
+    step_rads = (angle_degs * math.pi / 180)/n_frames
+
+    ids = canvas.find_withtag(tag)
+
+    for f in range(n_frames):   
+        for id in ids:
+            [ox1, oy1, ox2, oy2] = canvas.coords(id)
+            
+            (rx1, ry1) = _point_rotate(ox1,oy1,win_w*cx,win_h*cy,step_rads)
+            (rx2, ry2) = _point_rotate(ox2,oy2,win_w*cx,win_h*cy,step_rads)
+            canvas.coords(id, rx1, ry1, rx2, ry2)
+        
+        window.update()
+        time.sleep(1/fps)
+
+def _point_rotate(ox, oy, cx, cy, angle):
+    rx = cx + math.cos(angle) * (ox - cx) - math.sin(angle) * (oy - cy)
+    ry = cy + math.sin(angle) * (ox - cx) + math.cos(angle) * (oy - cy)
+
+    return (rx, ry)
+
 
 # Create the animaton
 window = create_window()
 canvas = create_canvas(window)
 
 # Displaying DNA sequences
-create_dna(canvas, ref_x1, ref_y1, ref_x2, ref_y2, fill=ref_seq_fill, width=dna_width, tags=["dna_lines","ref_seq"])
-create_text(canvas, 0.5, ref_yc, "Reference sequence", dna_font, tags=["dna_text","ref_seq","text"])
+create_text(canvas, 0.5, heading_y, "Loading cassette, reference and test sequences", heading_font, tags=["heading","text"])
+create_dna(canvas, ref_x1, ref_y1, ref_x2, ref_y2, fill=ref_seq_fill, width=dna_width, tags=["dna_lines","ref_seq_dna"])
+create_text(canvas, 0.5, ref_yc, "Reference sequence (sense)", dna_font, tags=["dna_text","ref_seq_text","text"])
 
-create_dna(canvas, cass_x1, cass_y1, cass_x2, cass_y2, fill=cass_seq_fill, width=dna_width, tags=["dna_lines","cass_seq"])
-create_text(canvas, 0.5, cass_yc, "Cassette sequence", dna_font, tags=["dna_text","cass_seq","text"])
+create_dna(canvas, cass_x1, cass_y1, cass_x2, cass_y2, fill=cass_seq_fill, width=dna_width, tags=["dna_lines","cass_seq_dna"])
+create_text(canvas, 0.5, cass_yc, "Cassette sequence (sense)", dna_font, tags=["dna_text","cass_seq_text","text"])
 
-create_dna(canvas, test_x1, test_y1, test_x2, test_y2, fill=test_seq_fill, width=dna_width, tags=["dna_lines","test_seq"])
-create_text(canvas, 0.5, test_yc, "Test sequence", dna_font, tags=["dna_text","test_seq","text"])
-
-# Displaying text
-create_text(canvas, 0.5, heading_y, "Finding cassette start in test sequence", heading_font, tags=["heading","text"])
-create_text(canvas, 0.5, sub_heading_y, "Extracting first N nucleotides of cassette", sub_heading_font, tags=["sub_heading"])
+create_dna(canvas, test_x1, test_y1, test_x2, test_y2, fill=test_seq_fill, width=dna_width, tags=["dna_lines","test_seq_dna"])
+create_text(canvas, 0.5, test_yc, "Test sequence (sense)", dna_font, tags=["dna_text","test_seq_text","text"])
 animate_pause(window,1)
 
 # Showing cassette search bubble
-create_bubble(canvas,cass_x1,cass_y1-bubble_h/2,cass_x1+bubble_w,cass_y1+bubble_h/2,bubble_outline=bubble_outline,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["bubble","cass_seq"])
-create_bubble(canvas,cass_x1,cass_y1-bubble_h/2,cass_x1+bubble_w,cass_y1+bubble_h/2,bubble_outline=bubble_outline,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["static_bubble","cass_seq"])
+canvas.delete("heading")
+create_text(canvas, 0.5, heading_y, "Finding cassette start in test sequence", heading_font, tags=["heading","text"])
+create_text(canvas, 0.5, sub_heading_y, "Extracting first N nucleotides of cassette", sub_heading_font, tags=["sub_heading"])
+create_bubble(canvas,cass_x1,cass_y1-bubble_h/2,cass_x1+bubble_w,cass_y1+bubble_h/2,bubble_outline=bubble_outline_sense,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["bubble"])
+create_bubble(canvas,cass_x1,cass_y1-bubble_h/2,cass_x1+bubble_w,cass_y1+bubble_h/2,bubble_outline=bubble_outline_sense,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["static_bubble","cass_seq_dna"])
 animate_pause(window,1)
 
 # Move search bubble to test sequence
@@ -131,17 +158,17 @@ animate_motion(window,canvas,cass_x1,cass_y1,test_x1,test_y1,step_size,"bubble")
 # Scan bubble along test sequence, highlighting matching sequences
 match_pos_1 = 0.3
 animate_motion(window,canvas,0.12,test_y1,match_pos_1,test_y1,step_size,"bubble")
-create_bubble(canvas,match_pos_1-bubble_h/2,test_y1-bubble_h/2,match_pos_1+bubble_w/2,test_y1+bubble_h/2,bubble_outline=bubble_outline,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["candidate","score_fail"])
+create_bubble(canvas,match_pos_1-bubble_h/2,test_y1-bubble_h/2,match_pos_1+bubble_w/2,test_y1+bubble_h/2,bubble_outline=bubble_outline_sense,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["candidate","score_fail"])
 create_text(canvas, match_pos_1, test_y1-0.05, "95% match", dna_font, tags=["score_text","score_fail"])
 
 match_pos_2 = 0.48
 animate_motion(window,canvas,match_pos_1,test_y1,match_pos_2,test_y1,step_size,"bubble")
-create_bubble(canvas,match_pos_2-bubble_h/2,test_y1-bubble_h/2,match_pos_2+bubble_w/2,test_y1+bubble_h/2,bubble_outline=bubble_outline,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["candidate","score_fail"])
+create_bubble(canvas,match_pos_2-bubble_h/2,test_y1-bubble_h/2,match_pos_2+bubble_w/2,test_y1+bubble_h/2,bubble_outline=bubble_outline_sense,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["candidate","score_fail"])
 create_text(canvas, match_pos_2, test_y1-0.05, "92% match", dna_font, tags=["score_text","score_fail"])
 
 match_pos_3 = 0.72
 animate_motion(window,canvas,match_pos_2,test_y1,match_pos_3,test_y1,step_size,"bubble")
-create_bubble(canvas,match_pos_3-bubble_h/2,test_y1-bubble_h/2,match_pos_3+bubble_w/2,test_y1+bubble_h/2,bubble_outline=bubble_outline,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["candidate","score_pass"])
+create_bubble(canvas,match_pos_3-bubble_h/2,test_y1-bubble_h/2,match_pos_3+bubble_w/2,test_y1+bubble_h/2,bubble_outline=bubble_outline_sense,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["candidate","score_pass"])
 create_text(canvas, match_pos_3, test_y1-0.05, "100% match", dna_font, tags=["score_text","score_pass"])
 
 animate_motion(window,canvas,match_pos_3,test_y1,test_x2,test_y1,step_size,"bubble")
@@ -154,6 +181,46 @@ create_text(canvas, 0.5, sub_heading_y, "Selecting match with highest score", su
 canvas.delete("score_fail")
 animate_pause(window,1)
 
+# Get reverse complement of cassette
+canvas.delete("sub_heading")
+create_text(canvas, 0.5, sub_heading_y, "Extracting first N nucleotides of cassette reverse complement", sub_heading_font, tags=["sub_heading"])
+animate_rotation(window,canvas,0.5,cass_yc,180,1,"cass_seq_dna")
+canvas.itemconfigure("cass_seq_text",text="Cassette sequence (RC)")
+create_bubble(canvas,cass_x1,cass_y1-bubble_h/2,cass_x1+bubble_w,cass_y1+bubble_h/2,bubble_outline=bubble_outline_rc,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["bubble"])
+create_bubble(canvas,cass_x1,cass_y1-bubble_h/2,cass_x1+bubble_w,cass_y1+bubble_h/2,bubble_outline=bubble_outline_rc,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["static_bubble","cass_seq_dna"])
+animate_pause(window,1)
+
+# Move search bubble to test sequence
+canvas.delete("sub_heading")
+create_text(canvas, 0.5, sub_heading_y, "Identifying matches for RC cassette start in test sequence", sub_heading_font, tags=["sub_heading"])
+animate_motion(window,canvas,cass_x1,cass_y1,test_x1,test_y1,step_size,"bubble")
+
+# Scan bubble along test sequence, highlighting matching sequences
+match_pos_1 = 0.2
+animate_motion(window,canvas,0.12,test_y1,match_pos_1,test_y1,step_size,"bubble")
+create_bubble(canvas,match_pos_1-bubble_h/2,test_y1-bubble_h/2,match_pos_1+bubble_w/2,test_y1+bubble_h/2,bubble_outline=bubble_outline_rc,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["candidate","score_pass_temp"])
+create_text(canvas, match_pos_1, test_y1-0.05, "98% match", dna_font, tags=["score_text","score_pass_temp"])
+
+match_pos_2 = 0.54
+animate_motion(window,canvas,match_pos_1,test_y1,match_pos_2,test_y1,step_size,"bubble")
+create_bubble(canvas,match_pos_2-bubble_h/2,test_y1-bubble_h/2,match_pos_2+bubble_w/2,test_y1+bubble_h/2,bubble_outline=bubble_outline_rc,bubble_width=bubble_width,dna_fill=cass_seq_fill,dna_width=dna_width,tags=["candidate","score_fail"])
+create_text(canvas, match_pos_2, test_y1-0.05, "94% match", dna_font, tags=["score_text","score_fail"])
+
+animate_motion(window,canvas,match_pos_2,test_y1,test_x2,test_y1,step_size,"bubble")
+canvas.delete("bubble")
+animate_pause(window,1)
+
+# Selecting match with highest score 
+canvas.delete("sub_heading")
+create_text(canvas, 0.5, sub_heading_y, "Selecting RC match with highest score", sub_heading_font, tags=["sub_heading"])
+canvas.delete("score_fail")
+animate_pause(window,1)
+
+# Selecting overall match with highest score 
+canvas.delete("sub_heading")
+create_text(canvas, 0.5, sub_heading_y, "Selecting overall match with highest score (sense and RC)", sub_heading_font, tags=["sub_heading"])
+canvas.delete("score_pass_temp")
+animate_pause(window,1)
 
 
 name = input('Press enter to end \n')
