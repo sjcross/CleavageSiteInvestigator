0ba3af61a51111a08e41e6c99f7b4a5b30f4caef
Stephen Cross <stephencross86@gmail.com>
2021-03-22 12:52:30 +0000

    Added tests for sequence splitting and orientation
    The splittest.py file will test a 6bp 5' OH, 6bp 3' OH and blunt end cut
    at all possible reference and sequence start/end points.  For each site,
    there are 4 variations covering the combinations of cassette and
    sequence orientation (sense or RC).

--- demo_csi.ps1
+++ demo_csi.ps1
@@ -12,8 +12,8 @@
 #     python .\src\main.py -c $cass_path -r $ref_path -t $_.FullName -sr -en 3
 # }
 
-$cass_path = "C:\Users\steph\Desktop\Oscar\Chloramphenicol Cassette overhang.fa"
-$ref_path = "C:\Users\steph\Desktop\Oscar\pUC19.fa"
+$cass_path = "D:\Stephen\People\Oscar\2020-02-20 Sequencing analysis\2021-02-23 Circularity test data\Cassette_200bp.fa"
+$ref_path = "D:\Stephen\People\Oscar\2020-02-20 Sequencing analysis\2021-02-23 Circularity test data\Ref_400bp.fa"
 
 # Running a single file
-python .\src\csi.py -c $cass_path -r $ref_path -t "C:\Users\steph\Desktop\Oscar\mix.fasta" -en 3 -rf "x>=3" -wi -ws
+python .\src\csi.py -c $cass_path -r $ref_path -t "D:\Stephen\People\Oscar\2020-02-20 Sequencing analysis\2021-02-23 Circularity test data\BE_loop.fa" -en 3 -pr
